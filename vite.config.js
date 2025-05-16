41e848fa7ce00907aca9f23041eec88a40d4be3a
YanniDoulamis <doulamis.yanni@gmail.com>
2025-05-16 15:11:43 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      //  👇  Hace que "@/..." apunte a la carpeta src
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   server: { port: 3000 }
 })
